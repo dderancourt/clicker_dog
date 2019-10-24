af3784c54c587e7f37b6219a1886d72ca19c5783
david derancourt <david.derancourt@hotmail.fr>
2019-10-24 11:40:32 +0200

    clicker function added

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -3,13 +3,31 @@ import React from "react";
 class Dog extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      love: 0,
+      lovePerSec: 0
+    };
+  }
+
+  clicker() {
+    let newLove = this.state.love + 1;
+    this.setState({ love: newLove });
   }
 
   render() {
     return (
       <div>
-        <img src="clickerDog.png" alt="Dog" />
+        <div>you have {this.state.love} love</div>
+        <div>you generate {this.state.lovePerSec} love per second</div>
+        <img
+          src="clickerDog.png"
+          alt="Dog"
+          onClick={event => {
+            {
+              this.clicker();
+            }
+          }}
+        />
       </div>
     );
   }
