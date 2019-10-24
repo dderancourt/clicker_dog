b4a861f0ef79741aae38a190fe4a324384739a95
Theo Boschetti <boschetti.theo@gmail.com>
2019-10-24 11:13:26 +0200

    TopBar component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import TopBar from './components/TopBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <TopBar />
     </div>
   );
 }
