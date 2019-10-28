b5a691af819e84a905e349ae30157c39a4c37422
Theo Boschetti <boschetti.theo@gmail.com>
2019-10-28 17:24:13 +0100

    new pictures

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -11,44 +11,46 @@ class Shop extends React.Component {
       <div className="shop">
         <div id="food">
           <button onClick={this.props.getFoodFromShop}>
-            <img src="food.png" alt="" />
+            <img src="food.png" alt="food" />
             <p>
-              Buy food (100 <img src="heartIcon.png"></img>)
+              Buy food (100 <img src="heartIcon.png" alt="heart"></img>)
             </p>
           </button>
         </div>
         <div id="toy">
           <button onClick={this.props.getToyFromShop}>
             <p>
-              Buy toy (1 000 <img src="heartIcon.png"></img>)
+              Buy toy (1 000 <img src="heartIcon.png" alt="heart"></img>)
             </p>
           </button>
         </div>
         <div id="basket">
           <button onClick={this.props.getBasketFromShop}>
             <p>
-              Buy basket (6 000 <img src="heartIcon.png"></img>)
+              Buy basket (6 000 <img src="heartIcon.png" alt="heart"></img>)
             </p>
           </button>
         </div>
         <div id="female-dog">
           <button onClick={this.props.getFemaleDogFromShop}>
             <p>
-              Buy female dog (20 000 <img src="heartIcon.png"></img>)
+              Buy female dog (20 000 <img src="heartIcon.png" alt="heart"></img>
+              )
             </p>
           </button>
         </div>
         <div id="slave-cat">
           <button onClick={this.props.getSlaveCatFromShop}>
             <p>
-              Buy slave cat (100 000 <img src="heartIcon.png"></img>)
+              Buy slave cat (100 000 <img src="heartIcon.png" alt="heart"></img>
+              )
             </p>
           </button>
         </div>
         <div id="parc">
           <button onClick={this.props.getParcFromShop}>
             <p>
-              Buy Parc (500 000 <img src="heartIcon.png"></img>)
+              Buy Parc (500 000 <img src="heartIcon.png" alt="heart"></img>)
             </p>
           </button>
         </div>
