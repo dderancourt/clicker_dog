879f62fb086a32f2d540cf0f1def465fd3097bc8
david derancourt <david.derancourt@hotmail.fr>
2019-10-28 17:34:30 +0100

    buying function added, item have now price

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -22,63 +22,81 @@ class Game extends React.Component {
   }
 
   getFoodFromShop = () => {
-    this.setState({
-      lovePerSecond: this.state.lovePerSecond + 1,
-      inventory: {
-        ...this.state.inventory,
-        food: this.state.inventory.food + 1
-      }
-    });
+    if (this.state.love >= 100) {
+      this.setState({
+        love: this.state.love - 100,
+        lovePerSecond: this.state.lovePerSecond + 1,
+        inventory: {
+          ...this.state.inventory,
+          food: this.state.inventory.food + 1
+        }
+      });
+    }
   };
 
   getToyFromShop = () => {
-    this.setState({
-      lovePerSecond: this.state.lovePerSecond + 10,
-      inventory: {
-        ...this.state.inventory,
-        toy: this.state.inventory.toy + 1
-      }
-    });
+    if (this.state.love >= 1000) {
+      this.setState({
+        love: this.state.love - 1000,
+        lovePerSecond: this.state.lovePerSecond + 10,
+        inventory: {
+          ...this.state.inventory,
+          toy: this.state.inventory.toy + 1
+        }
+      });
+    }
   };
 
   getBasketFromShop = () => {
-    this.setState({
-      lovePerSecond: this.state.lovePerSecond + 50,
-      inventory: {
-        ...this.state.inventory,
-        basket: this.state.inventory.basket + 1
-      }
-    });
+    if (this.state.love >= 6000) {
+      this.setState({
+        love: this.state.love - 6000,
+        lovePerSecond: this.state.lovePerSecond + 50,
+        inventory: {
+          ...this.state.inventory,
+          basket: this.state.inventory.basket + 1
+        }
+      });
+    }
   };
 
   getFemaleDogFromShop = () => {
-    this.setState({
-      lovePerSecond: this.state.lovePerSecond + 100,
-      inventory: {
-        ...this.state.inventory,
-        female_dog: this.state.inventory.female_dog + 1
-      }
-    });
+    if (this.state.love >= 20000) {
+      this.setState({
+        love: this.state.love - 20000,
+        lovePerSecond: this.state.lovePerSecond + 100,
+        inventory: {
+          ...this.state.inventory,
+          female_dog: this.state.inventory.female_dog + 1
+        }
+      });
+    }
   };
 
   getSlaveCatFromShop = () => {
-    this.setState({
-      lovePerSecond: this.state.lovePerSecond + 500,
-      inventory: {
-        ...this.state.inventory,
-        slave_cat: this.state.inventory.slave_cat + 1
-      }
-    });
+    if (this.state.love >= 100000) {
+      this.setState({
+        love: this.state.love - 100000,
+        lovePerSecond: this.state.lovePerSecond + 500,
+        inventory: {
+          ...this.state.inventory,
+          slave_cat: this.state.inventory.slave_cat + 1
+        }
+      });
+    }
   };
 
   getParcFromShop = () => {
-    this.setState({
-      lovePerSecond: this.state.lovePerSecond + 10000,
-      inventory: {
-        ...this.state.inventory,
-        parc: this.state.inventory.parc + 1
-      }
-    });
+    if (this.state.love >= 500000) {
+      this.setState({
+        love: this.state.love - 500000,
+        lovePerSecond: this.state.lovePerSecond + 10000,
+        inventory: {
+          ...this.state.inventory,
+          parc: this.state.inventory.parc + 1
+        }
+      });
+    }
   };
 
   getClickFromDog = () => {
